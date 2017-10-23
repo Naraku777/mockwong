6ad158297839838d02f3a63727fcb5004db5f0f6
naraku777 <442815900@qq.com>
2017-10-23 15:35:12 +0800

    Add vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,11 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// Vuetify
+import Vuetify from 'vuetify'
+import '../../node_modules/vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
